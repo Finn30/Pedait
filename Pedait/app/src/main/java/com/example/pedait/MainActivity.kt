01cd659ddb68334b54c9b7bd7cbd996233af1764
Finn30 <kevin304.kv@gmail.com>
2025-05-07 11:58:47 +0800

    Update Main Activity and 4 fragment

--- Pedait/app/src/main/java/com/example/pedait/MainActivity.kt
+++ Pedait/app/src/main/java/com/example/pedait/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
