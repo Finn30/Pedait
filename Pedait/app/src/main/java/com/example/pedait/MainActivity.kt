01233d1ee3f0b88674acb4312094359b1326b15f
Finn30 <kevin304.kv@gmail.com>
2025-05-12 14:11:42 +0800

    Update App

--- Pedait/app/src/main/java/com/example/pedait/MainActivity.kt
+++ Pedait/app/src/main/java/com/example/pedait/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pedait
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -12,14 +13,33 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.pedait.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    // Gedung A
+    private val allowedLocation = LatLng(-8.5871109, 116.0971896)
+
+    private val locationThreshold = 50
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +51,13 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
         val fab = findViewById<FloatingActionButton>(R.id.scanQRBtn)
+
         bottomNavigationView.background = null
+        bottomNavigationView.menu.findItem(R.id.placeholder).isEnabled = false
 
-        val menu = bottomNavigationView.menu
-        val placeholderItem = menu.findItem(R.id.placeholder)
-        placeholderItem.isEnabled = false
-
-        openFragment(HomeFragment())
+        handleNavigationIntent(intent)
 
         bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
@@ -52,7 +70,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         fab.setOnClickListener {
-            startActivity(Intent(this, ScanQRActivity::class.java))
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            checkUserLocationAndScan()
         }
 
     }
@@ -63,4 +82,156 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
+    private fun navigateToFragment(fragment: Fragment, menuItemId: Int) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.map_container, fragment)
+            .commit()
+        bottomNavigationView.selectedItemId = menuItemId
+    }
+
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleNavigationIntent(intent)
+    }
+
+    private fun handleNavigationIntent(intent: Intent) {
+        when (intent.getStringExtra("navigateTo")) {
+            "presence" -> navigateToFragment(PresenceFragment(), R.id.menuPresence)
+            "schedule" -> navigateToFragment(ScheduleFragment(), R.id.menuSchedule)
+            "profile" -> navigateToFragment(ProfileFragment(), R.id.menuProfile)
+            else -> navigateToFragment(HomeFragment(), R.id.menuHome)
+        }
+    }
+
+
+    private fun checkUserLocationAndScan() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Izin lokasi tidak diberikan, aktifkan di pengaturan", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                val userLatLng = LatLng(location.latitude, location.longitude)
+                val distance = calculateDistance(userLatLng, allowedLocation)
+
+                if (distance <= locationThreshold) {
+                    scannerLauncher.launch(
+                        ScanOptions().setPrompt("Scan QR Code").setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+                    )
+                } else {
+                    Toast.makeText(this, "Anda berada di luar area yang diizinkan", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Lokasi tidak ditemukan, coba lagi", Toast.LENGTH_LONG).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Gagal mendapatkan lokasi: ${it.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun calculateDistance(start: LatLng, end: LatLng): Float {
+        val results = FloatArray(1)
+        Location.distanceBetween(start.latitude, start.longitude, end.latitude, end.longitude, results)
+        return results[0]
+    }
+
+    private val scannerLauncher = registerForActivityResult<ScanOptions, ScanIntentResult>(
+        ScanContract()
+    ) { result ->
+        if (result.contents == null) {
+            Toast.makeText(this, "Scan dibatalkan", Toast.LENGTH_SHORT).show()
+        } else {
+            try {
+                val qrData = JSONObject(result.contents)
+                val sessionId = qrData.getString("session_id")
+                val lat = qrData.getDouble("lat")
+                val lng = qrData.getDouble("lng")
+                val radius = qrData.optInt("radius", 50)
+
+                // Ganti allowedLocation dengan lokasi dari QR
+                val newAllowedLocation = LatLng(lat, lng)
+
+                checkUserDistanceAndSendAttendance(newAllowedLocation, sessionId, radius)
+
+            } catch (e: Exception) {
+                Toast.makeText(this, "QR Code tidak valid", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun checkUserDistanceAndSendAttendance(qrLocation: LatLng, sessionId: String, radius: Int) {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                val userLatLng = LatLng(location.latitude, location.longitude)
+                val distance = calculateDistance(userLatLng, qrLocation)
+
+                if (distance <= radius) {
+                    sendAttendanceToServer(sessionId)
+                } else {
+                    Toast.makeText(this, "Anda berada di luar area yang diizinkan", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Lokasi tidak ditemukan, coba lagi", Toast.LENGTH_LONG).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Gagal mendapatkan lokasi: ${it.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun sendAttendanceToServer(sessionId: String) {
+        val studentId = "F1D022121"
+        val studentName = "Gevano Kevin Ravensy"
+
+        val json = JSONObject()
+        json.put("session_id", sessionId)
+        json.put("student_id", studentId)
+        json.put("student_name", studentName)
+
+
+        val requestBody = json.toString().toRequestBody("application/json".toMediaType())
+
+        val request = Request.Builder()
+            .url("http://10.70.1.8:5000/attended")
+            .post(requestBody)
+            .build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Presensi berhasil", Toast.LENGTH_SHORT).show()
+                        navigateToFragment(PresenceFragment(), R.id.menuPresence)
+                    }
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Gagal mengirim data presensi", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            checkUserLocationAndScan()
+        } else {
+            Toast.makeText(this, "Izin lokasi diperlukan untuk scan", Toast.LENGTH_LONG).show()
+        }
+    }
 }
